ba262c7c06ef8c33ffe0e15cc048f47c03ad1e86
Povilas Balciunas <balciunas90@gmail.com>
2017-03-08 20:19:40 +0200

    Implement character labelling in web app

--- web.py
+++ web.py
@@ -1,4 +1,9 @@
-from flask import render_template, Flask, send_from_directory
+import os
+import shutil
+
+from flask import render_template, Flask, send_from_directory, request
+
+import fs
 
 app = Flask(__name__)
 
@@ -10,12 +15,22 @@ def index():
 
 @app.route('/img/<int:img_id>')
 def render_image(img_id):
+    prev_result = request.args.get('content')
+    if prev_result:
+        classify_image(img_id - 1, prev_result)
     return render_template('show_image.html', img_id=img_id)
 
 
 @app.route('/chars/<filename>')
 def send_image(filename):
     return send_from_directory('chars', filename)
+
+
+def classify_image(img_id: int, label: str) -> None:
+    img_file = 'chars/{}.png'.format(img_id)
+    label_dir = 'labelled-chars/{}'.format(label)
+    os.makedirs(label_dir, exist_ok=True)
+    shutil.copyfile(img_file, fs.new_char_file(label_dir))
 
 
 if __name__ == '__main__':

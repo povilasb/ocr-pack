b10dcecb999186a3c3732b5a8167145cc6a5927b
Povilas Balciunas <balciunas90@gmail.com>
2017-03-08 21:07:51 +0200

    Allow to specify background color

--- main.py
+++ main.py
@@ -23,14 +23,16 @@ Rect = Tuple[int, int, int, int]
 
 
 class Image:
-    def __init__(self, array: np.ndarray) -> None:
+    def __init__(self, array: np.ndarray, background: int=255) -> None:
         self._array = array
+        self._background = background
+
         self.height = array.shape[0]
         self.width = array.shape[1]
 
     @classmethod
     def read_from(cls: 'Image', fname: str) -> 'Image':
-        return cls(io.imread(fname))
+        return cls(io.imread(fname), background=1)
 
     def save_to(self, fname: str) -> None:
         io.imsave(fname, self._array)
@@ -40,14 +42,14 @@ class Image:
         char_pos = self._array < 30
         bin_img = self._array.copy()
         bin_img[char_pos] = 0
-        bin_img[background_pos] = 255
+        bin_img[background_pos] = self._background
         return Image(bin_img)
 
     def rect(self, rect: Rect) -> 'Image':
         return Image(self._array[rect[0]:rect[2], rect[1]:rect[3]])
 
     def segments(self) -> Iterable['Image']:
-        labels = morphology.label(self._array, background=255)
+        labels = morphology.label(self._array, background=self._background)
         regions = measure.regionprops(labels)
         return map(lambda region: self.rect(region.bbox),
                    sorted(regions, key=lambda r: r.bbox[1]))
@@ -57,7 +59,7 @@ class Image:
         return Image(np.pad(
             self._array,
             ((0, new_height - height), (0, new_width - width)),
-            mode='constant', constant_values=255
+            mode='constant', constant_values=self._background
         ))
 
     def show(self) -> None:

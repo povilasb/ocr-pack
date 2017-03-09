7d6d9b1f2452004b68d23bf4dbcf9f169bd8ca4a
Povilas Balciunas <balciunas90@gmail.com>
2017-03-09 08:31:36 +0200

    Prepare images for scikit-learn training

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from typing import Tuple, Iterable
+from typing import Tuple, Iterable, List
 import os
 import itertools as it
 
 from skimage import io, filters, feature, segmentation, measure, morphology
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
 
 io.use_plugin('matplotlib')
 
@@ -23,16 +24,22 @@ Rect = Tuple[int, int, int, int]
 
 
 class Image:
-    def __init__(self, array: np.ndarray, background: int=255) -> None:
+    def __init__(self, array: np.ndarray, label: str='',
+                 background: int=255) -> None:
         self._array = array
         self._background = background
 
+        self.label = label
         self.height = array.shape[0]
         self.width = array.shape[1]
 
     @classmethod
     def read_from(cls: 'Image', fname: str) -> 'Image':
         return cls(io.imread(fname), background=1)
+
+    def with_label(self, label: str) -> 'Image':
+        self.label = label
+        return self
 
     def save_to(self, fname: str) -> None:
         io.imsave(fname, self._array)
@@ -43,7 +50,16 @@ class Image:
         bin_img = self._array.copy()
         bin_img[char_pos] = 0
         bin_img[background_pos] = self._background
-        return Image(bin_img)
+        return Image(bin_img, self.label)
+
+    def invert(self) -> 'Image':
+        """Only works with binary images."""
+        background_pos = self._array == self._background
+        foreground_pos = self._array == 0
+        new_img = self._array.copy()
+        new_img[foreground_pos] = self._background
+        new_img[background_pos] = 0
+        return Image(new_img, self.label)
 
     def rect(self, rect: Rect) -> 'Image':
         return Image(self._array[rect[0]:rect[2], rect[1]:rect[3]])
@@ -60,7 +76,7 @@ class Image:
             self._array,
             ((0, new_height - height), (0, new_width - width)),
             mode='constant', constant_values=self._background
-        ))
+        ), self.label, background=self._background)
 
     def show(self) -> None:
         io.imshow(self._array)
@@ -70,6 +86,10 @@ class Image:
 def images_in(dir_path: str) -> Iterable[Image]:
     return map(lambda fname: Image.read_from('{}/{}'.format(dir_path, fname)),
                os.listdir(dir_path))
+
+
+def labelled_images_in(dir_path: str, label: str) -> Iterable[Image]:
+    return map(lambda img: img.with_label(label), images_in(dir_path))
 
 
 def new_char_file(chars_dir: str='chars') -> str:
@@ -95,11 +115,46 @@ def max_char_size_in(imgs: Iterable[Image]) -> Tuple[int, int]:
     )
 
 
+def all_labelled_images() -> Iterable[Image]:
+    return it.chain.from_iterable([images_in('labelled-chars/{}'.format(char))
+        for char in os.listdir('labelled-chars')])
+
+
+class TrainingData:
+    def __init__(self) -> None:
+        self.max_height, self.max_width = max_char_size_in(
+            all_labelled_images())
+
+    def all_labelled_imgs(self) -> Tuple[List[np.ndarray], List[str]]:
+        imgs = []
+        labels = []
+        for char in os.listdir('labelled-chars'):
+            i, l = self.imgs_with_labels(char)
+            imgs += i
+            labels += l
+        return imgs, labels
+
+    def imgs_with_labels(self, char: str) -> Tuple[List[np.ndarray], List[str]]:
+        imgs = list(self.arrays_for(char))
+        return imgs, list(char * len(imgs))
+
+    def arrays_for(self, char: str) -> Iterable[np.ndarray]:
+        return map(lambda img: img._array.flatten(), self.imgs_for(char))
+
+    def imgs_for(self, char: str) -> Iterable[Image]:
+        return map(lambda img: img.extend_to(self.max_height, self.max_width)
+                   .invert(),
+                   labelled_images_in('labelled-chars/{}'.format(char), char))
+
+
 def main():
-    imgs = [images_in('labelled-chars/{}'.format(char))
-            for char in os.listdir('labelled-chars')]
-    max_height, max_width = max_char_size_in(it.chain.from_iterable(imgs))
-    print(max_height, max_width)
+    data = TrainingData()
+    print('Normalized size:', data.max_height, data.max_width)
+
+    imgs, labels = data.all_labelled_imgs()
+    clf = KNeighborsClassifier()
+    clf.fit(imgs, labels)
+    print(clf.predict(imgs[labels.index('X')]))
 
 
 main()

53b2b930d0c434925f5edaa98dcbf9135178d9b6
Povilas Balciunas <balciunas90@gmail.com>
2017-03-11 18:16:51 +0200

    Imove image class to separate graphics module

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from skimage import io, filters, feature, segmentation, measure, morphology
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
+from gfx import Image
+
 io.use_plugin('matplotlib')
 
 
@@ -18,79 +20,6 @@ io.use_plugin('matplotlib')
 
 
 # bbox: (y1, x1, y2, x2)
-
-
-Rect = Tuple[int, int, int, int]
-
-
-class Image:
-    def __init__(self, array: np.ndarray, label: str='',
-                 background: int=255) -> None:
-        self._array = array
-        self._background = background
-
-        self.label = label
-        self.height = array.shape[0]
-        self.width = array.shape[1]
-
-    @classmethod
-    def read_from(cls: 'Image', fname: str, background=255) -> 'Image':
-        img = io.imread(fname)
-        return cls(img, background=background)
-
-    @classmethod
-    def read_from_array(cls: 'Image', fname: str, background=1) -> 'Image':
-        return cls(np.load(fname), background=background)
-
-    def with_label(self, label: str) -> 'Image':
-        self.label = label
-        return self
-
-    def with_background(self, background: int) -> 'Image':
-        new_img = self._array.copy()
-        new_img[new_img == self._background] = background
-        return Image(new_img, self.label, background)
-
-    def save_to(self, fname: str) -> None:
-        np.save(fname, self._array, allow_pickle=False)
-
-    def binary(self, threshold: int=30) -> 'Image':
-        background_pos = self._array >= threshold
-        char_pos = self._array < threshold
-        bin_img = self._array.copy()
-        bin_img[char_pos] = 0
-        bin_img[background_pos] = self._background
-        return Image(bin_img, self.label)
-
-    def invert(self) -> 'Image':
-        """Only works with binary images."""
-        background_pos = self._array == self._background
-        foreground_pos = self._array == 0
-        new_img = self._array.copy()
-        new_img[foreground_pos] = self._background
-        new_img[background_pos] = 0
-        return Image(new_img, self.label)
-
-    def rect(self, rect: Rect) -> 'Image':
-        return Image(self._array[rect[0]:rect[2], rect[1]:rect[3]])
-
-    def segments(self) -> Iterable['Image']:
-        labels = morphology.label(self._array, background=self._background)
-        regions = measure.regionprops(labels)
-        return map(lambda region: self.rect(region.bbox),
-                   sorted(regions, key=lambda r: r.bbox[1]))
-
-    def extend_to(self, new_height: int, new_width: int) -> 'Image':
-        height, width = self._array.shape
-        return Image(np.pad(
-            self._array,
-            ((0, new_height - height), (0, new_width - width)),
-            mode='constant', constant_values=self._background
-        ), self.label, background=self._background)
-
-    def show(self) -> None:
-        io.imshow(self._array)
-        io.show()
 
 
 def images_in(dir_path: str) -> Iterable[Image]:

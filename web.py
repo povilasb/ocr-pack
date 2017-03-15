ea42aefe9f0c46137a09b2f4b433b8a13e3d8736
Povilas Balciunas <balciunas90@gmail.com>
2017-03-15 07:39:55 +0200

    Store labelled characters as numpy arrays

--- web.py
+++ web.py
@@ -1,9 +1,9 @@
 import os
-import shutil
 
 from flask import render_template, Flask, send_from_directory, request
 
 import fs
+import gfx
 
 app = Flask(__name__)
 
@@ -30,7 +30,7 @@ def classify_image(img_id: int, label: str) -> None:
     img_file = 'chars/{}.png'.format(img_id)
     label_dir = 'labelled-chars/{}'.format(label)
     os.makedirs(label_dir, exist_ok=True)
-    shutil.copyfile(img_file, fs.new_char_file(label_dir))
+    gfx.Image.read_from(img_file).save_to(fs.new_char_file(label_dir))
 
 
 if __name__ == '__main__':

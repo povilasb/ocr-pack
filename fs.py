c22445822b7befd6be03e4a114b21e9181d4f869
Povilas Balciunas <balciunas90@gmail.com>
2017-09-13 14:09:06 +0300

    Add missing file system module

---
+++ fs.py
@@ -0,0 +1,13 @@
+import os
+import json
+from typing import Union
+
+
+def new_char_file(chars_dir: str='chars') -> str:
+    chars = os.listdir(chars_dir)
+    return '{}/{}.npy'.format(chars_dir, len(chars) or 0)
+
+
+def read_json_from(fname: str) -> Union[dict, list]:
+    with open(fname, 'r') as f:
+        return json.load(f)
